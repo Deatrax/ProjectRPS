d7526ab178fbc7c63ac750ec872cb7a5f334b76d
Didhiti0217 <didhiti.nahid@gmail.com>
2026-02-10 14:52:25 +0600

    add task skeleton

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,15 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './index.css';
 import PandaLamp from './components/PandaLamp';
+import AddTask from './components/AddTask';
 
 function App() {
   return (
     <div>
-      {/* Navigation Links */}
-      <nav>
-        <Link to="/login">Go to Login</Link>
-      </nav>
-
       <Routes>
         {/* Change path="/" to path="*" to match all sub-routes */}
-        <Route path="*" element={<PandaLamp />} />
+        <Route path="/" element={<PandaLamp />} />
+        <Route path="/AddTask" element={<AddTask />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/AddTask.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AddTask() {
+  return (
+    <div>
+      <h2>Add Task</h2>
+      {/* Add your form or UI for adding a task here */}
+    </div>
+  );
+}
