82600e3f8c51e34c6bfa7b190caf5fa144d8c2b4
Deatrax <161139217+Deatrax@users.noreply.github.com>
2026-02-22 02:57:50 +0600

    set the default page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')).render(
         <Routes>
           {/* Define your routes here */}
           {/* Public Routes */}
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
 
